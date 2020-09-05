f2b6ac5e515ea98e5082709941e9a20a35425d9a
I Dewa Putu Ardi Nusawan <ardi.nusawan13@gmail.com>
2020-09-05 16:37:20 +0800

    Update function so caller can specify how many times the character repeated

--- learn-go-with-test/iteration/repeat.go
+++ learn-go-with-test/iteration/repeat.go
@@ -1,9 +1,7 @@
 package iteration
 
-const repeatCount = 5
-
 // Repeat get character and return repeated character for 5 times.
-func Repeat(character string) string {
+func Repeat(character string, repeatCount int) string {
 	var repeated string
 	for i := 0; i < repeatCount; i++ {
 		repeated += character
