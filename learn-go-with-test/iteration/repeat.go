85a0f99d9f44e491594f5dabbdb21cad9e188e3d
I Dewa Putu Ardi Nusawan <ardi.nusawan13@gmail.com>
2020-09-05 16:43:31 +0800

    Use standart lib repeat

--- learn-go-with-test/iteration/repeat.go
+++ learn-go-with-test/iteration/repeat.go
@@ -1,10 +1,8 @@
 package iteration
 
+import "strings"
+
 // Repeat get character and return repeated character for 5 times.
 func Repeat(character string, repeatCount int) string {
-	var repeated string
-	for i := 0; i < repeatCount; i++ {
-		repeated += character
-	}
-	return repeated
+	return strings.Repeat(character, repeatCount)
 }
