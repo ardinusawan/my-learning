3a5c8d03d84f4ed60c7f91f96087c819a12aab12
I Dewa Putu Ardi Nusawan <ardi.nusawan13@gmail.com>
2020-09-06 15:06:36 +0800

    Use range instead of for loop

--- learn-go-with-test/arrays_and_slices/sum.go
+++ learn-go-with-test/arrays_and_slices/sum.go
@@ -3,8 +3,8 @@ package main
 // Sum get array and return calculated sum for every value inside of it.
 func Sum(numbers [5]int) int {
 	sum := 0
-	for i := 0; i < 5; i++ {
-		sum += numbers[i]
+	for _, number := range numbers {
+		sum += number
 	}
 	return sum
 }
