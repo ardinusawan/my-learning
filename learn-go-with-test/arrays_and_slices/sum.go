2da8e2e21ec58c02ae3b15244eac1abf2cd2f839
I Dewa Putu Ardi Nusawan <ardi.nusawan13@gmail.com>
2020-09-06 17:06:41 +0800

    Create new func SumAll

--- learn-go-with-test/arrays_and_slices/sum.go
+++ learn-go-with-test/arrays_and_slices/sum.go
@@ -8,3 +8,15 @@ func Sum(numbers []int) int {
 	}
 	return sum
 }
+
+// SumAll take slices of array and return calculated all value inside of it.
+func SumAll(numbersToSum ...[]int) []int {
+	lengthOfNumbers := len(numbersToSum)
+	sums := make([]int, lengthOfNumbers)
+
+	for i, numbers := range numbersToSum {
+		sums[i] = Sum(numbers)
+	}
+
+	return sums
+}
