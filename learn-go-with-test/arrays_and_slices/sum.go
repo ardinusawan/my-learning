35672e78c9d8e5abb8da2c7548ba3705fa6f681f
I Dewa Putu Ardi Nusawan <ardi.nusawan13@gmail.com>
2020-09-06 15:25:21 +0800

    Use slice instead use fix array

--- learn-go-with-test/arrays_and_slices/sum.go
+++ learn-go-with-test/arrays_and_slices/sum.go
@@ -1,7 +1,7 @@
 package main
 
 // Sum get array and return calculated sum for every value inside of it.
-func Sum(numbers [5]int) int {
+func Sum(numbers []int) int {
 	sum := 0
 	for _, number := range numbers {
 		sum += number
