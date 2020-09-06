6086a5894160bef529524716d715ad3a52e5cc4c
I Dewa Putu Ardi Nusawan <ardi.nusawan13@gmail.com>
2020-09-06 15:02:28 +0800

    Add sum func

---
+++ learn-go-with-test/arrays_and_slices/sum.go
@@ -0,0 +1,10 @@
+package main
+
+// Sum get array and return calculated sum for every value inside of it.
+func Sum(numbers [5]int) int {
+	sum := 0
+	for i := 0; i < 5; i++ {
+		sum += numbers[i]
+	}
+	return sum
+}
