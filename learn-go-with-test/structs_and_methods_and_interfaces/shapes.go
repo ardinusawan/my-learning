9eb615d50ea3c3a0df7f4a21488d292ddb779396
I Dewa Putu Ardi Nusawan <ardi.nusawan13@gmail.com>
2020-09-08 11:19:24 +0800

    Add perimeter file

---
+++ learn-go-with-test/structs_and_methods_and_interfaces/shapes.go
@@ -0,0 +1,6 @@
+package main
+
+// Perimeter take width and height and return calculated the perimeter of a rectangle
+func Perimeter(width float64, height float64) float64 {
+	return 2 * (width + height)
+}
