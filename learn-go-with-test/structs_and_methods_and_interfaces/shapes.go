7b36961d3a4b800337a19264e79174f6d8125423
I Dewa Putu Ardi Nusawan <ardi.nusawan13@gmail.com>
2020-09-12 16:47:11 +0800

    Use struct as argument in func

--- learn-go-with-test/structs_and_methods_and_interfaces/shapes.go
+++ learn-go-with-test/structs_and_methods_and_interfaces/shapes.go
@@ -1,11 +1,17 @@
 package main
 
+// Rectangle element
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+
 // Perimeter take width and height and return calculated the perimeter of a rectangle
-func Perimeter(width float64, height float64) float64 {
-	return 2 * (width + height)
+func Perimeter(rectangle Rectangle) float64 {
+	return 2 * (rectangle.Width + rectangle.Height)
 }
 
 // Area take width and height and return area of rectangle
-func Area(width float64, height float64) float64 {
-	return width * height
+func Area(rectangle Rectangle) float64 {
+	return rectangle.Width * rectangle.Height
 }
