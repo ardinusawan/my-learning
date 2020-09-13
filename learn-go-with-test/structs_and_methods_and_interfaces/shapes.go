58be3b75f4312b86be419cf468e4bcaaa980ff54
I Dewa Putu Ardi Nusawan <ardi.nusawan13@gmail.com>
2020-09-13 15:10:13 +0800

    Using method to calculate Area

--- learn-go-with-test/structs_and_methods_and_interfaces/shapes.go
+++ learn-go-with-test/structs_and_methods_and_interfaces/shapes.go
@@ -1,17 +1,27 @@
 package main
 
+import "math"
+
 // Rectangle element
 type Rectangle struct {
 	Width  float64
 	Height float64
 }
 
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
+}
+
+// Circle element
+type Circle struct {
+	Radius float64
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
+}
+
 // Perimeter take width and height and return calculated the perimeter of a rectangle
 func Perimeter(rectangle Rectangle) float64 {
 	return 2 * (rectangle.Width + rectangle.Height)
-}
-
-// Area take width and height and return area of rectangle
-func Area(rectangle Rectangle) float64 {
-	return rectangle.Width * rectangle.Height
 }
