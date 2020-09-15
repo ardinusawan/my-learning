178d340bf36ceba782fa9a9e73e30c1e7fe7097f
I Dewa Putu Ardi Nusawan <ardi.nusawan13@gmail.com>
2020-09-15 23:56:09 +0800

    Create new type data Bitcoin which is int; Use Stringer to override print methods of object

--- learn-go-with-test/pointers_and_errors/wallet.go
+++ learn-go-with-test/pointers_and_errors/wallet.go
@@ -4,18 +4,29 @@ import (
 	"fmt"
 )
 
+// Stringer will override print methods of object
+type Stringer interface {
+	String() string
+}
+
+// Bitcoin is int type data
+type Bitcoin int
+
 // Wallet type data
 type Wallet struct {
-	balance int
+	balance Bitcoin
 }
 
 // Deposit will adds amount of money to wallet
-func (w *Wallet) Deposit(amount int) {
-	fmt.Printf("address of balance in Deposit is %v \n", &w.balance)
+func (w *Wallet) Deposit(amount Bitcoin) {
 	w.balance += amount
 }
 
 // Balance will print total of money on wallet
-func (w *Wallet) Balance() int {
+func (w *Wallet) Balance() Bitcoin {
 	return w.balance
+}
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
 }
