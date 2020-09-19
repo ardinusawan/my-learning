6eac2701808e7c5ad441762f99f13640a6c39abf
I Dewa Putu Ardi Nusawan <ardi.nusawan13@gmail.com>
2020-09-19 17:15:20 +0800

    When add to existing key throw error and dont update definition

--- learn-go-with-test/maps/dictionary.go
+++ learn-go-with-test/maps/dictionary.go
@@ -7,6 +7,7 @@ import (
 type Dictionary map[string]string
 
 var ErrorNotFound = errors.New("could not find the word you were looking for")
+var ErrWordExist = errors.New("word already exist")
 
 func (d Dictionary) Search(word string) (string, error) {
 	definition, ok := d[word]
@@ -16,6 +17,17 @@ func (d Dictionary) Search(word string) (string, error) {
 	return definition, nil
 }
 
-func (d Dictionary) Add(word, definition string) {
-	d[word] = definition
+func (d Dictionary) Add(word, definition string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrorNotFound:
+		d[word] = definition
+	case nil:
+		return ErrWordExist
+	default:
+		return err
+	}
+
+	return nil
 }
