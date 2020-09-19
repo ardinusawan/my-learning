6723293ae4625a64645203ab94f0306b96189bee
I Dewa Putu Ardi Nusawan <ardi.nusawan13@gmail.com>
2020-09-19 17:30:55 +0800

    Group constant var

--- learn-go-with-test/maps/dictionary.go
+++ learn-go-with-test/maps/dictionary.go
@@ -6,8 +6,10 @@ import (
 
 type Dictionary map[string]string
 
-var ErrorNotFound = errors.New("could not find the word you were looking for")
-var ErrWordExist = errors.New("word already exist")
+var (
+	ErrorNotFound = errors.New("could not find the word you were looking for")
+	ErrWordExist  = errors.New("word already exist")
+)
 
 func (d Dictionary) Search(word string) (string, error) {
 	definition, ok := d[word]
