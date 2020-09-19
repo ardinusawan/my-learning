f508de26fdf9d32886c4e1bb1625bce50303b3d5
I Dewa Putu Ardi Nusawan <ardi.nusawan13@gmail.com>
2020-09-19 16:26:24 +0800

    Adding function Add to add new key-value

--- learn-go-with-test/maps/dictionary_test.go
+++ learn-go-with-test/maps/dictionary_test.go
@@ -22,6 +22,22 @@ func TestSearch(t *testing.T) {
 	})
 }
 
+func TestAdd(t *testing.T) {
+	dictionary := Dictionary{}
+	dictionary.Add("test", "this is just a test")
+
+	want := "this is just a test"
+	got, err := dictionary.Search("test")
+
+	if err != nil {
+		t.Fatal("should find added word:", err)
+	}
+
+	if got != want {
+		t.Errorf("got %q want %q", want, got)
+	}
+}
+
 func assertStrings(t *testing.T, got, want string) {
 	t.Helper()
 
