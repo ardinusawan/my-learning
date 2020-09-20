93265596de981f59db57229d06a5cedec186b280
I Dewa Putu Ardi Nusawan <ardi.nusawan13@gmail.com>
2020-09-20 16:07:00 +0800

    When update and word not exist throw ErrWordDoesNotExist

--- learn-go-with-test/maps/dictionary.go
+++ learn-go-with-test/maps/dictionary.go
@@ -3,8 +3,9 @@ package main
 type Dictionary map[string]string
 
 const (
-	ErrorNotFound = DictionaryErr("could not find the word you were looking for")
-	ErrWordExist  = DictionaryErr("word already exist")
+	ErrorNotFound       = DictionaryErr("could not find the word you were looking for")
+	ErrWordExist        = DictionaryErr("word already exist")
+	ErrWordDoesNotExist = DictionaryErr("cannot update the word because it does not exist")
 )
 
 type DictionaryErr string
@@ -36,6 +37,17 @@ func (d Dictionary) Add(word, definition string) error {
 	return nil
 }
 
-func (d Dictionary) Update(word, definition string) {
-	d[word] = definition
+func (d Dictionary) Update(word, definition string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrorNotFound:
+		return ErrWordDoesNotExist
+	case nil:
+		d[word] = definition
+	default:
+		return err
+	}
+
+	return nil
 }
