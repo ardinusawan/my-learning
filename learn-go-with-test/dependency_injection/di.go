c8e5016abef0d60e3caf688d58a51fcf08427496
I Dewa Putu Ardi Nusawan <ardi.nusawan13@gmail.com>
2020-09-20 22:24:12 +0800

    Create func Greet can reuse because it inject io.Writer

--- learn-go-with-test/dependency_injection/di.go
+++ learn-go-with-test/dependency_injection/di.go
@@ -1,10 +1,21 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
+	"io"
+	"net/http"
+	// "os"
 )
 
-func Greet(writer *bytes.Buffer, name string) {
+func Greet(writer io.Writer, name string) {
 	fmt.Fprintf(writer, "Hello, %s", name)
+}
+
+func MyGreeterHandler(w http.ResponseWriter, r *http.Request) {
+	Greet(w, "world")
+}
+
+func main() {
+	http.ListenAndServe(":5000", http.HandlerFunc(MyGreeterHandler))
+	// Greet(os.Stdout, "Elodie")
 }
