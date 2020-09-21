04dc18576892668fd096f25696e374af8be7e025
I Dewa Putu Ardi Nusawan <ardi.nusawan13@gmail.com>
2020-09-21 14:21:30 +0800

    Add configurable Sleept

--- learn-go-with-test/mocking/countdown_test.go
+++ learn-go-with-test/mocking/countdown_test.go
@@ -4,10 +4,19 @@ import (
 	"bytes"
 	"reflect"
 	"testing"
+	"time"
 )
 
 type CountdownOperationsSpy struct {
 	Calls []string
+}
+
+type SpyTime struct {
+	durationSlept time.Duration
+}
+
+func (s *SpyTime) Sleep(duration time.Duration) {
+	s.durationSlept = duration
 }
 
 func (s *CountdownOperationsSpy) Sleep() {
@@ -57,4 +66,16 @@ Go!`
 			t.Errorf("got %q want %q", got, want)
 		}
 	})
+}
+
+func TestConfigurableSleeper(t *testing.T) {
+	sleepTime := 5 * time.Second
+
+	spyTime := &SpyTime{}
+	sleeper := ConfigurableSleeper{sleepTime, spyTime.Sleep}
+	sleeper.Sleep()
+
+	if spyTime.durationSlept != sleepTime {
+		t.Errorf("should have slept for %v but slept for %v", sleepTime, spyTime.durationSlept)
+	}
 }
