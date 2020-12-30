552810dd43072fcda23866aeaa9330e312cbabd8
I Dewa Putu Ardi Nusawan <ardi.nusawan13@gmail.com>
2020-12-30 20:03:06 +0800

    Remove unused import

--- learn-go-with-test/maths/clockface.go
+++ learn-go-with-test/maths/clockface.go
@@ -1,1 +1,14 @@
-package maths
+package clockface
+
+import (
+	"time"
+)
+
+type Point struct {
+	X float64
+	Y float64
+}
+
+func SecondHand(t time.Time) Point {
+	return Point{150, 60}
+}
--- learn-go-with-test/maths/clockface_test.go
+++ learn-go-with-test/maths/clockface_test.go
@@ -1,17 +1,15 @@
-package maths
+package clockface
 
 import (
 	"testing"
 	"time"
-
-	"github.com/quii/learn-go-with-tests/math/vFinal/clockface"
 )
 
 func TestSecondHandAtMidnight(t *testing.T) {
 	tm := time.Date(1337, time.January, 1, 0, 0, 0, 0, time.UTC)
 
-	want := clockface.Point{X: 150, Y: 150 - 90}
-	got := clockface.SecondHand(tm)
+	want := Point{X: 150, Y: 150 - 90}
+	got := SecondHand(tm)
 
 	if got != want {
 		t.Errorf("Got %v, wanted %v", got, want)
