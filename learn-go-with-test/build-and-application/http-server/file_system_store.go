762817d0e969d7b66a48965f552d98b327422fdb
I Dewa Putu Ardi Nusawan <ardi.nusawan13@gmail.com>
2021-05-18 11:49:06 +0800

    Work with empty file

--- learn-go-with-test/build-and-application/http-server/file_system_store.go
+++ learn-go-with-test/build-and-application/http-server/file_system_store.go
@@ -11,8 +11,26 @@ type FileSystemPlayerStore struct {
 	league   League
 }
 
-func NewFileSystemPlayerStore(file *os.File) (*FileSystemPlayerStore, error) {
+func initializePlayerDBFile(file *os.File) error {
 	file.Seek(0, 0)
+	info, err := file.Stat()
+	if err != nil {
+		return fmt.Errorf("problem getting file info from file %s, %v", file.Name(), err)
+	}
+	if info.Size() == 0 {
+		file.Write([]byte("[]"))
+		file.Seek(0, 0)
+	}
+
+	return nil
+}
+
+func NewFileSystemPlayerStore(file *os.File) (*FileSystemPlayerStore, error) {
+	err := initializePlayerDBFile(file)
+	if err != nil {
+		return nil, fmt.Errorf("problem initialising player db file, %v", err)
+	}
+
 	league, err := NewLeague(file)
 
 	if err != nil {
--- learn-go-with-test/build-and-application/http-server/file_system_store_test.go
+++ learn-go-with-test/build-and-application/http-server/file_system_store_test.go
@@ -74,6 +74,15 @@ func TestFileSystemStore(t *testing.T) {
 		want := 1
 		assertScoreEquals(t, got, want)
 	})
+
+	t.Run("work with empty file", func(t *testing.T) {
+		database, cleanDatabase := createTempFile(t, "")
+		defer cleanDatabase()
+
+		_, err := NewFileSystemPlayerStore(database)
+
+		assertNoError(t, err)
+	})
 }
 
 func createTempFile(t testing.TB, initialData string) (*os.File, func()) {
